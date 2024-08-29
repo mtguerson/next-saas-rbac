4c4004244d4261fbc8f77482cdd843a6310074d0
mtguerson <mtguerson@gmail.com>
2024-08-29 14:03:52 -0400

    feat: add optimization and typed permissions

--- packages/auth/src/subjects/user.ts
+++ packages/auth/src/subjects/user.ts
@@ -1,1 +1,13 @@
-export type UserSubject = ['create' | 'delete' | 'invite' | 'manage', 'User']
+import { z } from 'zod'
+
+export const userSubject = z.tuple([
+  z.union([
+    z.literal('manage'),
+    z.literal('get'),
+    z.literal('update'),
+    z.literal('delete'),
+  ]),
+  z.literal('Project'),
+])
+
+export type UserSubject = z.infer<typeof userSubject>
