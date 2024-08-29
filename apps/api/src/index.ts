b393e92ece2cfdcb608e7c3fbfb44d336aabf896
mtguerson <mtguerson@gmail.com>
2024-08-29 13:22:04 -0400

    feat: add role permissions

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,7 +1,9 @@
-import { ability } from '@saas/auth'
+import { defineAbilityFor } from '@saas/auth'
+
+const ability = defineAbilityFor({ role: 'ADMIN' })
 
 const userCanInviteSomeoneElse = ability.can('invite', 'User')
-const userCanDeleteOtherUsers = ability.cannot('delete', 'User')
+const userCanDeleteOtherUsers = ability.can('delete', 'User')
 
 console.log(userCanInviteSomeoneElse)
 console.log(userCanDeleteOtherUsers)
