b8ebf248a995ef810d6f43a52cfeb95c2a0053ed
mtguerson <mtguerson@gmail.com>
2024-09-04 10:29:40 -0400

    route: create projects

---
+++ apps/api/src/http/routes/projects/create-project.ts
@@ -0,0 +1,68 @@
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { z } from 'zod'
+
+import { auth } from '@/http/middlewares/auth'
+import { prisma } from '@/lib/prisma'
+import { createSlug } from '@/utils/create-slug'
+import { getUserPermissions } from '@/utils/get-users-permissions'
+
+import { UnauthorizedError } from '../_errors/unauthorized-error'
+
+export async function createProject(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .post(
+      '/organizations/:slug/projects',
+      {
+        schema: {
+          tags: ['projects'],
+          summary: 'Create a new project',
+          security: [{ bearerAuth: [] }],
+          body: z.object({
+            name: z.string(),
+            description: z.string(),
+          }),
+          params: z.object({
+            slug: z.string(),
+          }),
+          response: {
+            201: z.object({
+              projectId: z.string().uuid(),
+            }),
+          },
+        },
+      },
+      async (request, reply) => {
+        const { slug } = request.params
+        const userId = await request.getCurrentUserId()
+        const { membership, organization } =
+          await request.getUserMembership(slug)
+
+        const { cannot } = getUserPermissions(userId, membership.role)
+
+        if (cannot('create', 'Project')) {
+          throw new UnauthorizedError(
+            `You're not allowed to create new projects.`,
+          )
+        }
+
+        const { name, description } = request.body
+
+        const project = await prisma.project.create({
+          data: {
+            name,
+            slug: createSlug(name),
+            description,
+            organizationId: organization.id,
+            ownerId: userId,
+          },
+        })
+
+        return reply.status(201).send({
+          projectId: project.id,
+        })
+      },
+    )
+}
--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -25,6 +25,7 @@ import { getOrganizations } from './routes/orgs/get-organizations'
 import { shutdownOrganization } from './routes/orgs/shutdown-organization'
 import { transferOrganization } from './routes/orgs/transfer-organization'
 import { updateOrganization } from './routes/orgs/update-organization'
+import { createProject } from './routes/projects/create-project'
 // import { getMembership } from './routes/orgs/get-membership'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
@@ -78,6 +79,8 @@ app.register(getOrganizations)
 app.register(updateOrganization)
 app.register(shutdownOrganization)
 app.register(transferOrganization)
+
+app.register(createProject)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('HTTP server running! 🎈')
