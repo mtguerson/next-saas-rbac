3bf93f9fbe2b044a1c6f9b9ccb17bb1d1d6ab758
mtguerson <mtguerson@gmail.com>
2024-09-04 11:33:30 -0400

    fix: change method

--- apps/api/src/http/routes/projects/update-project.ts
+++ apps/api/src/http/routes/projects/update-project.ts
@@ -14,7 +14,7 @@ export async function updateProject(app: FastifyInstance) {
   app
     .withTypeProvider<ZodTypeProvider>()
     .register(auth)
-    .delete(
+    .put(
       '/organizations/:slug/projects/:projectId',
       {
         schema: {
