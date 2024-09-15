3402e93e98e45642987f251a117e84ff2b80b6a7
mtguerson <mtguerson@gmail.com>
2024-09-14 21:19:40 -0400

    feat: organization-switcher component

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -1,7 +1,9 @@
+import { Slash } from 'lucide-react'
 import Image from 'next/image'
 
 import donutsCake from '@/assets/donuts-cake.svg'
 
+import { OrganizationSwitcher } from './organization-switcher'
 import { ProfileButton } from './profile-button'
 
 export function Header() {
@@ -9,6 +11,10 @@ export function Header() {
     <div className="mx-auto flex max-w-[1200px] items-center justify-between">
       <div className="flex items-center gap-3">
         <Image src={donutsCake} className="size-6 dark:invert" alt="Donuts" />
+
+        <Slash className="text-bordere size-4 -rotate-[24deg]" />
+
+        <OrganizationSwitcher />
       </div>
 
       <div className="flex items-center gap-4">
