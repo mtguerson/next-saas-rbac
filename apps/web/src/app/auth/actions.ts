cd98b941487da4c5ad52e8e9727ad32d19f05dae
mtguerson <mtguerson@gmail.com>
2024-09-16 11:40:27 -0400

    env: utilizate envs at client

--- apps/web/src/app/auth/actions.ts
+++ apps/web/src/app/auth/actions.ts
@@ -1,14 +1,15 @@
 'use server'
 
+import { env } from '@saas/env'
 import { redirect } from 'next/navigation'
 
 export async function signInWithGithub() {
   const githubSignInUrl = new URL('login/oauth/authorize', 'https://github.com')
 
-  githubSignInUrl.searchParams.set('client_id', 'Ov23liEHT0kRckHUzyii')
+  githubSignInUrl.searchParams.set('client_id', env.GITHUB_OAUTH_CLIENT_ID)
   githubSignInUrl.searchParams.set(
     'redirect_uri',
-    'http://localhost:3000/api/auth/callback',
+    env.GITHUB_OAUTH_CLIENT_REDIRECT_URI,
   )
   githubSignInUrl.searchParams.set('scope', 'user')
 
