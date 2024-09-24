e9845bddf5c83b46cc9f312d4efd98bb9c77a163
mtguerson <mtguerson@gmail.com>
2024-09-24 14:48:59 -0400

    feat: accept invite

---
+++ apps/web/src/http/get-invite.ts
@@ -0,0 +1,26 @@
+import type { Role } from '@saas/auth'
+
+import { api } from './api-client'
+
+interface GetInviteResponse {
+  invite: {
+    organization: {
+      name: string
+    }
+    id: string
+    createdAt: string
+    role: Role
+    email: string
+    author: {
+      name: string | null
+      id: string
+      avatarUrl: string | null
+    } | null
+  }
+}
+
+export async function getInvite(inviteId: string) {
+  const result = await api.get(`invites/${inviteId}`).json<GetInviteResponse>()
+
+  return result
+}
